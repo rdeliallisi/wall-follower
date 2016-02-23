aa8c48059e37f8f5b4f30db23cd25e6620a74758
Siddharth Shukla <siddharthshukla@outlook.com>
2016-02-23 16:22:09 +0100

    Oops, forgot to commit the better sized chart

--- Design Phase/presentation/assets/gannt/example.js
+++ Design Phase/presentation/assets/gannt/example.js
@@ -1,8 +1,8 @@
 var tasks = [
 {"startDate":new Date("Tue Feb 09 00:00:01 EST 2016"),"endDate":new Date("Tue Feb 23 00:00:01 EST 2016"),"taskName":"Design presentation","status":"SUCCEEDED"},
-{"startDate":new Date("Tue Feb 23 00:00:01 EST 2016"),"endDate":new Date("Tue Mar 8 00:00:01 EST 2016"),"taskName":"Low level control module","status":"RUNNING"},
-{"startDate":new Date("Tue Mar 08 00:00:01 EST 2016"),"endDate":new Date("Tue Mar 15 00:00:01 EST 2016"),"taskName":"High level control module","status":"RUNNING"},
-{"startDate":new Date("Tue Mar 15 00:00:01 EST 2016"),"endDate":new Date("Tue Apr 05 00:00:01 EST 2016"),"taskName":"Object Detection module","status":"RUNNING"},
+{"startDate":new Date("Tue Feb 23 00:00:01 EST 2016"),"endDate":new Date("Tue Mar 8 00:00:01 EST 2016"),"taskName":"Object Detection module","status":"RUNNING"},
+{"startDate":new Date("Tue Mar 08 00:00:01 EST 2016"),"endDate":new Date("Tue Mar 15 00:00:01 EST 2016"),"taskName":"Low level control module","status":"RUNNING"},
+{"startDate":new Date("Tue Mar 15 00:00:01 EST 2016"),"endDate":new Date("Tue Apr 05 00:00:01 EST 2016"),"taskName":"High level control module","status":"RUNNING"},
 {"startDate":new Date("Tue Apr 05 00:00:01 EST 2016"),"endDate":new Date("Tue Apr 19 23:59:59 EST 2016"),"taskName":"Mission control","status":"RUNNING"},
 {"startDate":new Date("Tue Apr 19 00:00:01 EST 2016"),"endDate":new Date("Tue Apr 26 23:59:59 EST 2016"),"taskName":"First Version (alpha)","status":"RUNNING"},
 {"startDate":new Date("Tue Apr 26 00:00:01 EST 2016"),"endDate":new Date("Tue May 03 23:59:59 EST 2016"),"taskName":"Fully tested final version","status":"RUNNING"}
@@ -14,7 +14,7 @@ var taskStatus = {
     "RUNNING" : "bar-running",
     "KILLED" : "bar-killed"
 };
-var taskNames = ["Fully tested final version", "First Version (alpha)", "Mission control", "Object Detection module", "High level control module", "Low level control module", "Design presentation"];
+var taskNames = ["Fully tested final version", "First Version (alpha)", "Mission control", "High level control module", "Low level control module", "Object Detection module", "Design presentation"];
 taskNames.reverse()
 tasks.sort(function(a, b) {
     return a.endDate - b.endDate;
