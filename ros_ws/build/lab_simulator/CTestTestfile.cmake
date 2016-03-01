3879a347e217ebf231243daf7c7c36a454210ca6
Rubin Deliallisi <r.deliallisi@jacobs-university.de>
2016-03-01 15:09:29 +0100

    Base Structure

--- ros_ws/build/lab_simulator/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/rubin/ros_ws/src/lab_simulator
-# Build directory: /home/rubin/ros_ws/build/lab_simulator
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
