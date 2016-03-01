87460be9e8c6226de54e551cbe56f0963db20593
thunderboltsid <s.shukla@jacobs-university.de>
2016-03-01 23:50:30 +0000

    Log more stuff

--- Packages/robot/src/CircleDetector.cpp
+++ Packages/robot/src/CircleDetector.cpp
@@ -4,7 +4,18 @@
 #include <sstream>
 
 void callback(const sensor_msgs::LaserScan& msg) {
-    ROS_INFO("%f\n", msg.scan_time);
+    ROS_INFO("Angle Min: %f\n", msg.angle_min);
+    ROS_INFO("Angle max: %f\n", msg.angle_max);
+    ROS_INFO("Angle increment: %f\n", msg.angle_increment);
+    ROS_INFO("time increment: %f\n", msg.time_increment);
+    ROS_INFO("Scan Time: %f\n", msg.scan_time);
+    ROS_INFO("range Min: %f\n", msg.range_min);
+    ROS_INFO("range max: %f\n", msg.range_max);
+    for (int i = 0; i < sizeof(msg.ranges)/ sizeof(float) ; ++i) {
+        ROS_INFO("range %i: %f\n", i, msg.ranges[i]);
+        ROS_INFO("intensity %i: %f\n", i, msg.intensities[i]);
+    }
+    ROS_INFO("--------------------------------------------------------------------");
 }
 
 int main(int argc, char **argv) {
