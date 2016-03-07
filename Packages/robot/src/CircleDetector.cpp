bd422f90d4def28b63e5c360f90c3ae72f33193a
m.ficiu@jacobs-university.de <m.ficiu@jacobs-university.de>
2016-03-07 23:34:26 +0100

    Added circle detection

--- Packages/robot/src/CircleDetector.cpp
+++ Packages/robot/src/CircleDetector.cpp
@@ -1,28 +1,101 @@
 #include "ros/ros.h"
 #include "sensor_msgs/LaserScan.h"
 
-#include <sstream>
+#include "math.h"
 
+#include <sstream>
+#include <opencv2/core/core.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <vector>
+
+#define number_of_values 719
+
+using namespace std;
+using namespace cv;
+
+//TODO(atabakhafeez): Check and return correct values for coordinates
+/**
+* Gets the data from the laser range finder, creates an 
+* image out of it and runs openCV HoughLines on it
+* @param msg Raw data comming from the laser range finder
+*/
 void callback(const sensor_msgs::LaserScan& msg) {
-    ROS_INFO("Angle Min: %f\n", msg.angle_min);
-    ROS_INFO("Angle max: %f\n", msg.angle_max);
-    ROS_INFO("Angle increment: %f\n", msg.angle_increment);
-    ROS_INFO("time increment: %f\n", msg.time_increment);
-    ROS_INFO("Scan Time: %f\n", msg.scan_time);
-    ROS_INFO("range Min: %f\n", msg.range_min);
-    ROS_INFO("range max: %f\n", msg.range_max);
-    for (int i = 0; i < sizeof(msg.ranges)/ sizeof(float) ; ++i) {
-        ROS_INFO("range %i: %f\n", i, msg.ranges[i]);
-        ROS_INFO("intensity %i: %f\n", i, msg.intensities[i]);
+
+    //create image
+    int screen_width = 1000;
+    int screen_height = 1000;
+    cv::Mat image;
+    image.create(screen_width, screen_height, CV_8UC1);
+    for(int i = 0; i < image.rows; i++){
+        for(int j = 0; j < image.cols; j++){
+        image.at<uchar>(i,j) = (uchar)255;
+        }
     }
-    ROS_INFO("--------------------------------------------------------------------");
+                        
+    //convert laser_scan data to image
+    float base_scan_min_angle = -2.094395;
+    for (int i = 0; i < number_of_values; ++i) {
+        //calculate cartesian coordinates
+        float cartesian_x = (msg.ranges[i] * sin(base_scan_min_angle)) * 100;
+        float cartesian_y = (msg.ranges[i] * cos(base_scan_min_angle)) * 100;
+        base_scan_min_angle += 0.005826;
+
+        //convert to screen coordinates
+        int screen_x = (int)((cartesian_x + screen_width/2));
+        int screen_y = (int)((-cartesian_y + screen_height/2));
+
+        if (screen_x > 0 && screen_y > 0) {
+            image.at<uchar>(screen_y, screen_x) = (uchar)0;
+        } else {
+            cout << screen_x << endl;
+            cout << screen_y << endl;
+        }
+    }
+
+    //TODO(atabakhafeez): Delete code below. Only used for visual help.
+    //namedWindow( "Display window", WINDOW_AUTOSIZE );
+    //imshow( "Display window", image );
+    //waitKey(0); 
+
+    //compute Hough Transform
+    cv::Mat destination;
+    cv::Canny(image, destination, 50, 200, 3);
+
+    vector<Vec4i> lines;
+    HoughLinesP( destination, lines, 1, CV_PI/180, 100, 30, 10 );
+
+    vector<Vec3f> circles;
+    HoughCircles(image, circles, CV_HOUGH_GRADIENT, 1, image.rows/4, 200, 10, 0, 200 );
+
+    cout << "number of lines = " << lines.size() << endl;
+    cout << "number of circles = " << circles.size() << endl;
+
+    //TODO(atabakhfeez): Check if conversions of coordinates to original form are correct
+    for (int i = 0; i < lines.size(); ++i){
+        cout << (lines.at(i)[0])/100 << endl;
+        float scaled_distance = (lines.at(i)[0])/100;
+        float original_x = scaled_distance * cos(lines.at(i)[1]) - 5;
+        float original_y = scaled_distance * sin(lines.at(i)[1]) - 5;
+
+        float point_distance = sqrt(original_x * original_x + original_y * original_y);
+        cout << "distance = " << point_distance << endl;
+
+        cout << lines.at(i) << endl;
+    }
+
+    for (int i = 0; i < circles.size(); ++i) {
+        cout << circles.at(i) << endl;
+    }
 }
 
 int main(int argc, char **argv) {
-    ros::init(argc, argv, "circle_detector");
+    ros::init(argc, argv, "line_detector");
     ros::NodeHandle n;
 
+    ros::Rate loop_rate(50);
     ros::Subscriber sub = n.subscribe("base_scan", 1000, callback);
     ros::spin();
+    loop_rate.sleep();
     return 0;
 }
