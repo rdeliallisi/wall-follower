9be8a076c76524e610309594ffa7ad532fbaa6be
Rubin Deliallisi <r.deliallisi@jacobs-university.de>
2016-03-08 17:02:10 +0100

    Remove documentation. To be created on develop branch

--- Documentation/source_documentation/html/search/functions_1.js
+++
@@ -1,6 +0,0 @@
-var searchData=
-[
-  ['is_5fclose_5fto_5fwall',['is_close_to_wall',['../classHighLevelControl.html#ab958c89bc1762671d14b967b15dccfb8',1,'HighLevelControl']]],
-  ['is_5ffollowing_5fwall',['is_following_wall',['../classHighLevelControl.html#acfa4985eb2022acd8b59886657877acf',1,'HighLevelControl']]],
-  ['is_5fturning',['is_turning',['../classHighLevelControl.html#a72337bf999829a3fc0752bdfe419f97d',1,'HighLevelControl']]]
-];
--- Documentation/source_documentation/html/search/functions_4.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['wallfollowmove',['WallFollowMove',['../classHighLevelControl.html#ab1d5093f7707ff78368af39f5db38575',1,'HighLevelControl']]]
-];
