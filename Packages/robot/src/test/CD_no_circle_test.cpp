e37feadb8cd192cd6bdb874c3c5ffd161efbce93
Rubin Deliallisi <r.deliallisi@jacobs-university.de>
2016-03-08 16:52:56 +0100

    Working Test Cases

--- Packages/robot/src/test/CD_no_circle_test.cpp
+++ Packages/robot/src/test/CD_no_circle_test.cpp
@@ -2,10 +2,17 @@
 #include <ros/ros.h>
 #include "circle_detector.h"
 
-
-
 TEST(CircleDetectTest, CircleDetected) {
 	CircleDetector circle_detector;
+
+	ros::Rate r(10.0);
+	int i = 0;
+	while (ros::ok() && i < 10) {
+		ros::spinOnce();
+		i++;
+		r.sleep();
+	}
+
 	ASSERT_FLOAT_EQ(circle_detector.get_circle().x, -10);
 	ASSERT_FLOAT_EQ(circle_detector.get_circle().y, -10);
 }
