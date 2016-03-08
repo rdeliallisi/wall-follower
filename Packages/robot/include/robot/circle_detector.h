114b4befaf6d5f35d6e3a9ba8dcb7ce3a7c4dcb9
Rubin Deliallisi <r.deliallisi@jacobs-university.de>
2016-03-08 20:42:03 +0100

    Added doxygen main page and fixed circle_detector.h comments

--- Packages/robot/include/robot/circle_detector.h
+++ Packages/robot/include/robot/circle_detector.h
@@ -5,59 +5,54 @@
 #include "sensor_msgs/LaserScan.h"
 
 /**
- * @brief Define the Circle class which 
- * takes the coordinates of the points.
- * @
- * 
+ * @brief Define the Circle class which takes the coordinates of the points.
  */
-class Circle{
+class Circle {
 public:
+
     /**
-     *  x is the x coordinate of the point 
-     *  relative to the laser range scanner
-     *  y is the y coordinate of the point
-     *  relative to the laser range scanner
+     * x is the x coordinate of the point relative to the laser range scanner
      */
-    float x, y;
+    float x;
+
+    /**
+     * y is the y coordinate of the point relative to the laser range scanner
+     */
+    float y;
 };
 
 /**
  * @brief Defines the CircleDetector class
  * which will help checking if a circle is found or not.
- * @details The results from the laser range scanner 
- * are used to check if the obstacle that is seen is 
+ * 
+ * @details The results from the laser range scanner
+ * are used to check if the obstacle that is seen is
  * in fact a circle.
  * Also, it returns information about the position of the circle
- * which is helpful in detecting it. 
- * 
+ * which is helpful in detecting it.
+ *
  */
 class CircleDetector {
 private:
-/**
- * @brief The class has as parameters the following:
- */
-    /*
-    @ brief The node that was created.
+    /**
+     * The node that was created.
      */
-
     ros::NodeHandle node_;
-    /*
-     @ brief The laser_sub subscribes the node that is seen 
-     to the laser range finder.
-    */
 
+    /**
+     * The laser_sub subscribes the node that is seen to the laser range finder.
+     */
     ros::Subscriber laser_sub_;
-    /*
-     * @brief The circle is the actual circle that the scanner
-        has found.
+
+    /**
+     * The circle is the actual circle that the scanner has found.
      */
 
     Circle circle_;
 
-    /*
-     * @brief  The count_threshold takes care of the 
-       thresholds that were encountered by the laser range
-       scanner so far.
+    /**
+     * The count_threshold takes care of the thresholds that were encountered by the 
+     * laser range scanner so far.
      */
     int count_threshold_;
 
@@ -68,7 +63,7 @@ public:
     /**
     * Gets the data from the laser range finder, creates an
     * image out of it and runs openCV HoughLines on it
-    * 
+    *
     * @param msg Raw data comming from the laser range finder
     */
     void LaserCallback(const sensor_msgs::LaserScan::ConstPtr& msg);
--- project_run.sh
+++
@@ -1,7 +0,0 @@
-catkin_make
-source devel/setup.bash
-roscore &
-wait(5)
-roslaunch lab_simulator simulator.launch &
-rosrun robot HighLevelControlNode &
-rosrun robot CircleDetector
