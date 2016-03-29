fd4ae44ab2a725a62935c6510e3800fd55488b86
Siddharth Shukla <siddharthshukla@outlook.com>
2016-03-29 14:52:27 +0200

    add utils test file for CD

---
+++ Packages/robot/test/CD_utils_test.cpp
@@ -0,0 +1,12 @@
+/**
+ * @file circle_detector.cpp
+ * @brief This file contains the unit tests for utility methods in CircleDetetor class
+ *
+ * @author Atabak Hafeez [atabakhafeez]
+ * @author Maria Ficiu [MariaFiciu]
+ * @author Rubin Deliallisi [rdeliallisi]
+ * @author Siddharth Shukla [thunderboltsid]
+ * @bug No known bugs.
+ */
+
+ #include <ros/ros.h>
